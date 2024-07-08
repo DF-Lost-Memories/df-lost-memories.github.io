3935aa4301dfc4af9a0b93fbdb3788c2016d3df7
HighFlyer222 <hflyer222@protonmail.com>
2024-07-08 16:38:01 +0200

    Use the size property of the textbox generator instead of resizing the images manually

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 export function generateTextboxes() {
     const textboxes = document.querySelectorAll(".textbox")
     textboxes.forEach(textbox => {
-        textbox.src = `https://demirramon.com/utgen.png?message=${encodeURIComponent(textbox.dataset.text)}`
+        textbox.src = `https://demirramon.com/utgen.png?message=${encodeURIComponent(textbox.dataset.text)}&size=2`
         textbox.src = textbox.src.replace("%5Cn", "%0A")
         textbox.alt = textbox.dataset.text.replace("\n", " * ")
-        textbox.onload = function () {
-            textbox.style.minWidth = textbox.width * 2 + "px"
-        }
-        textbox.onload()
     })
 }
 
